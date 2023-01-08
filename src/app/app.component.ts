da234c9370a90f02228a9d9c6473bda8acf836ba
kai1973i <kai1973i@googlemail.com>
2023-01-08 20:36:01 +0100

    gh-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+import * as THREE from "three";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'three_with_ang';
+  @ViewChild("canvasElement", {
+    static: true
+  })
+  canvasElementRef!: ElementRef<HTMLCanvasElement>;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+      const renderer = new THREE.WebGLRenderer({
+        canvas: this.canvasElementRef.nativeElement
+      });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      window.addEventListener("resize", () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        camera.updateMatrixWorld();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      });
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      function animate() {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+      }
+
+      animate();
+    });    
+  }
 }
